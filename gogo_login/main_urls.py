f70e108333c442902f972b4c4b580ef1f49b2b9e
byundojin <103907292+byundojin@users.noreply.github.com>
2023-05-15 20:39:45 +0900

    1

--- gogo_login/main_urls.py
+++ gogo_login/main_urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import main_views
 
 urlpatterns = [
-    path('minigame/<int:game>', main_views.minigame_page.as_view()),
+    path('minigame', main_views.minigame_page.as_view()),
     path('bet/<int:game>', main_views.bet_page.as_view()),
 ]
